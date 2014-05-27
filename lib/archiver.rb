5b20861cfc4997831b0e1b08dfb4915b1a0138ec
M. C. McGrath <shidash@shidash.com>
2014-05-26 20:43:00 -0700

    Archives websites in html and pdf files

---
+++ lib/archiver.rb
@@ -0,0 +1,74 @@
+require 'nokogiri'
+require 'pdfkit'
+require 'open-uri'
+require 'fileutils'
+require 'json'
+
+class Archiver
+  def initialize(type)
+    @type = type
+  end
+
+  # Archive a single url
+  def archiveone(url)
+    begin
+      html = Nokogiri::HTML(open(url))
+      url.gsub!("https", "http")
+      k = PDFKit.new(url, :page_size => "Letter")
+      pdf = k.to_pdf
+
+      # Save files
+      FileUtils::mkdir_p 'public/uploads/archive'
+      filepath = "public/uploads/archive/"+url.gsub("http", "https").gsub("/", "").gsub(".", "").gsub(":", "")+".pdf"
+      File.open(filepath.gsub(".pdf", ".html"), 'w') { |file| file.write(html) }
+      file = k.to_file(filepath)
+    
+      # Save output if single url
+      if @type == "single"
+        @output = Hash.new
+        @output[:pdf_path] = filepath
+        @output[:html_path] = filepath.gsub(".pdf", ".html")
+        @output[:text] = html.text
+      end
+
+      # Return file paths
+      out = Hash.new
+      out[:pdf_path] = filepath
+      out[:html_path] = filepath.gsub(".pdf", ".html")
+      return out
+    rescue
+    end
+  end
+
+  # Archive multiple fields in a json
+  def multiarchive(json, field)
+    pjson = JSON.parse(json)
+    htmlfield = field+"_htmlpath"
+    pdffield = field+"_pdfpath"
+    @output = Array.new
+
+    pjson.each do |p|
+      pitem = Hash.new
+      paths = archiveone(p[field])
+
+      # Save paths
+      if !(paths == nil)
+        pitem[htmlfield] = paths[:pdf_path]
+        pitem[pdffield] = paths[:html_path]
+      end
+      
+      # Save other fields
+      p.each do |key, value|
+        pitem[key] = value
+      end
+
+      @output.push(pitem)
+    end
+  end
+  
+  # Generate JSON from output
+  def genJSON
+    JSON.pretty_generate(@output)
+  end
+end
+
---
+++ urlarchiver.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name        = 'urlarchiver'
+  s.version     = '0.0.1'
+  s.date        = '2014-05-26'
+  s.summary     = "Archives websites."
+  s.description = "Saves html and pdfs of websites."
+  s.authors     = ["M. C. McGrath"]
+  s.email       = 'shidash@shidash.com'
+  s.files       = Dir["lib/*.rb"]
+  s.homepage    =
+    'https://github.com/TransparencyToolkit/Archiver'
+  s.license       = 'GPL'
+end
